1abd27e1666a4d1059b3954ba735dcdad1bbd170
danil.khandys <danil.khandys@xpage.ru>
2019-05-23 16:07:38 +0500

    sd234

--- src/ts/common.ts
+++ src/ts/common.ts
@@ -2,15 +2,17 @@ import {App, Element} from "./app"
 
 App.domReady(() => {
 	;(function(){
-		let maxHeight = 0;
+		window.addEventListener("load", function(){
+			let maxHeight = 0;
 
-		App.each(".cat-item__img", (el: HTMLElement, i: number) => {
-			if (maxHeight < parseInt(getComputedStyle(el).height))
-				maxHeight = parseInt(getComputedStyle(el).height)
-		})
+			App.each(".cat-item__img", (el: HTMLElement, i: number) => {
+				if (maxHeight < parseInt(getComputedStyle(el).height))
+					maxHeight = parseInt(getComputedStyle(el).height)
+			})
 
-		App.each(".cat-item__img", (el: HTMLElement, i: number) => {
-			el.style.height = (maxHeight + "px")
+			App.each(".cat-item__img", (el: HTMLElement, i: number) => {
+				el.style.height = (maxHeight + "px")
+			})
 		})
 	})()
 })
